dfd04f3af52d706232aebb11a67482ac45cb950c
Paolo Gatti <lordkrandel@gmail.com>
2016-02-02 23:55:15 +0100

    Aggiunto Gabri

--- Gioco.java
+++ Gioco.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Gioco;
+ package Gioco;
 
 public class Gioco {
     public static void main(String argomenti[]){
        Personaggio davide;
        davide = new Personaggio("Davide");
+       Personaggio gabri;
+       gabri = new Personaggio("Nelly");
 
        System.out.println(davide.nome);
     }
