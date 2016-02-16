600d02bc36473b741011bad6f1b5f7023eb45880
Paolo Gatti <lordkrandel@gmail.com>
2016-02-16 23:56:27 +0100

    Visualizzazione nemico Visualizzazione personaggi per scelta

--- Gioco.java
+++ Gioco.java
@@ -1,12 +1,60 @@
  package Gioco;
 
 public class Gioco {
-    public static void main(String argomenti[]){
-       Personaggio davide;
-       davide = new Personaggio("Davide");
-       Personaggio gabri;
-       gabri = new Personaggio("Nelly");
 
-       System.out.println(davide.nome);
+    public static void main(String argomenti[]){
+
+    /*    
+        Personaggio davide;
+        davide = new Personaggio("Davide");
+        Personaggio gabri;
+        gabri = new Personaggio("Nelly");
+        System.out.println(davide.nome);
+    */
+
+        Personaggio nemico; 
+        nemico = new Personaggio("Callos") ;
+        nemico.amico = false;
+        nemico.esperienza = 69955;
+        nemico.vita = 3000;
+        nemico.mana = 100;
+
+        System.out.println("ECCOLO!!! É ARRIVATO " + nemico.nome);
+        System.out.println("Livello: " + nemico.getlivello() );   
+        System.out.println("HP: " + nemico.vita);
+        System.out.println("MP: " + nemico.mana);
+
+        Personaggio amico; 
+        amico = new Personaggio("Vash") ;
+        amico.amico = true;
+        amico.esperienza = 5000;
+        amico.vita = 9999;
+        amico.mana = 999;
+        amico.professione = Professione.GUERRIERO;
+        
+        System.out.println("Seleziona:\n" + amico.nome);
+        System.out.println(amico.professione);   
+        System.out.println("Livello: " + amico.getlivello() );   
+        System.out.println("HP: " + amico.vita);
+        System.out.println("MP: " + amico.mana);
+       
+        Personaggio amico2; 
+        amico2 = new Personaggio("Franco", true, 700, 7781, 700);
+        amico2.professione = Professione.LADRO;
+
+        System.out.println(amico2.nome);
+        System.out.println("Livello: " + amico2.getlivello() );   
+        System.out.println(amico2.professione);   
+        System.out.println("HP: " + amico2.vita);
+        System.out.println("MP: " + amico2.mana);
     }
+
+
+
+
+
+
+
+
+
 }
--- Personaggio.java
+++ Personaggio.java
@@ -8,16 +8,51 @@ public class Personaggio {
     public Professione professione;
     public Sesso sesso;
     public Razza razza;
+    public boolean amico;
     public int vita;
     public int mana;
     public int soldi;
-    public int posizione_x;
-    public int posizione_y;
+    private int posizione_x;
+    private int posizione_y;
     public ArrayList<Magia> libroMagie;
     public ArrayList<Artefatto> artefatti;
 
     public Personaggio(String a_nome){
         this.nome = a_nome;
+    }
+    
+    /**
+     * Costruttore del personaggio.
+     * Serve ad inizializzare le caratteristiche principali
+     * 
+     * @param a_nome nome del personaggio
+     * @param a_amico amico o nemico
+     * @param a_esperienza esperienza
+     * @param a_vita vita
+     * @param a_mana mana
+    */
+    public Personaggio(String a_nome, boolean a_amico, int a_esperienza, int a_vita, int a_mana){
+        this.nome = a_nome;
+        this.amico = a_amico;
+        this.esperienza = a_esperienza;
+        this.vita = a_vita;
+        this.mana = a_mana;
+    }
+    
+    public int getlivello (){
+        int l_livello;
+        if (this.esperienza < 100) {
+            l_livello = 1;
+        } else if (this.esperienza < 500) {
+            l_livello = 2;
+        } else if (this.esperienza < 1000) {
+            l_livello = 3;
+        } else if (this.esperienza < 2000) {
+            l_livello = 4;
+        } else  {
+            l_livello = 5;
+        }
+        return l_livello;
     }
     
 }
